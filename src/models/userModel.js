499a37f26b6d88203b723e0456e66d9be5914c34
Shravan K Subrahmanya <shravanksubrahmanya@gmail.com>
2025-06-06 11:37:50 +0530

    Written code fr userModel

---
+++ src/models/userModel.js
@@ -0,0 +1,36 @@
+import mongoose, { mongo } from "mongoose";
+import { unique } from "next/dist/build/utils";
+import { type } from "os";
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: [true, "Please provide a username"],
+    unique: true,
+  },
+  email: {
+    type: email,
+    required: [true, "Please provide an email"],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide password"],
+  },
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
+  forgotPasswordToken: String,
+  forgotPasswordTokenExpiry: Date,
+  verifyToken: String,
+  verifyTokenExpiry: Date,
+});
+
+const User = mongoose.models.users || mongoose.model("users", userSchema);
+
+export default User;
